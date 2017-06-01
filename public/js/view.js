41a3850712d313136b9879bf1db83c4dec069bc4
lyl1988 <122391887@qq.com>
2017-05-31 20:51:19 -0700

    add sidebar

--- public/js/view.js
+++ public/js/view.js
@@ -19,16 +19,22 @@ $(window).on('scroll', function() {
     });
 });
 
-nav.find('a').on('click', function() {
-    var $el = $(this),
-        id = $el.attr('href');
 
-    $('html, body').animate({
-        scrollTop: $(id).offset().top - nav_height
-    }, 500);
 
-    return false;
-});
+var colorChoices = [
+    { 'choice': '#ed0a71' }, 
+    { 'choice': '#ef4036' }, 
+    { 'choice': '#f7941e' }, 
+    { 'choice': '#fcb040' }, 
+    { 'choice': '#fdef00' }, 
+    { 'choice': '#8cc63f' }, 
+    { 'choice': '#0b9444' }, 
+    { 'choice': '#13a89e' }, 
+    { 'choice': '#0084cb' }, 
+    { 'choice': '#46549e' }, 
+    { 'choice': '#662d91' }, 
+    { 'choice': '#92278f' }
+    ];
 
 
 
@@ -38,10 +44,12 @@ $(window).load(function() {
         $(".nav-container").toggleClass("showNav hideNav").removeClass("hidden");
         $(this).toggleClass("animated");
     });
+
+        for (var i = 0; i < 12; i++) {
+        $(".nav-list").append("<li class='list-item' style='background-color:" +colorChoices[i].choice+  "'><a></a></li>");
+    }
+
 });
 
 
 
-$(window).load(function() {
-    $("html body").css("background-color", "#768888");
-});
